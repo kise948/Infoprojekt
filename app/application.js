f83749ff0fcd84fabe02381de8f51583d06d9ff2
Jan <jan.akisuna@gmail.com>
2023-05-29 14:32:05 +0200

    js base

---
+++ app/application.js
@@ -0,0 +1,11 @@
+function main(){
+    let d3 = require("d3")
+    let labwork = require("../json files/labwork.json")
+    let courses = require("../json files/courses.json")
+    let degrees = require("../json files/degrees.json")
+    let repCardEntry = require("../json files/REPORT_CARD_ENTRY.json")
+    let repCardEntryType = require("../json files/REPORT_CARD_ENTRY_TYPE.json")
+
+
+
+}
