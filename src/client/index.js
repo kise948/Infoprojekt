367e6b6a798660330b49af5743d1b7da5c450e37
Jan <jan.akisuna@gmail.com>
2024-04-25 22:51:29 +0200

    repo clean up

--- src/client/index.js
+++ src/client/index.js
@@ -9,11 +9,12 @@ document.getElementById('query2-button').addEventListener('click', () => execute
 
 let labworkUUID = "5a81c332-34d9-4ced-b46d-3f02d1ce905d";
 let labworkUUIDs = ['5df25fb9-d52d-445a-b6ea-017893c533ac', '003d4a10-3357-4ff1-aab1-92d9688f1193', '34043dd9-a72b-4328-8731-b8d061e10e33', '7aa0c375-6906-4a44-b7cc-bc906789c496', '5a81c332-34d9-4ced-b46d-3f02d1ce905d']
+
 // In the actual implementation, the labworkUUID would be included in the labwork's site, where the graphics would be available
 async function executeQuery(queryNumber) {
     console.log('Query number: ', queryNumber);
-    const urls = [`http://localhost:3000/api/anmeldungen-vs-bestanden?labworkUUID=${labworkUUID}`,
-        `http://localhost:3000/api/AnmeldungenUndTeilnamenAnMeilensteinen?labworkUUID=${labworkUUID}`];
+    const urls = [`http://localhost:3000/api/anmeldungen-vs-bestanden/${labworkUUID}`,
+        `http://localhost:3000/api/anmeldungen-und-teilnahmen/${labworkUUID}`];
     const url = urls[queryNumber-1];
     const response = await fetch(url);
     if (!response.ok) {
